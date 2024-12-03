02c5842dd6662c2a2510e617c6a2aee50dfb3cf2
Norhan-salem <norhansalem581@gmail.com>
2024-12-03 19:13:38 +0200

    added notification prefernces

---
+++ lib/data/services/preference_service.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferencesService {
+  static const _notificationsKey = 'notifications_enabled';
+
+  Future<bool> getNotificationPreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_notificationsKey) ?? true; // Default is notif enabled
+  }
+
+  Future<bool> setNotificationPreference(bool isEnabled) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setBool(_notificationsKey, isEnabled);
+  }
+}
