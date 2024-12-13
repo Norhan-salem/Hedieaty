6f5b9fb2ab5ba54026e043630fb56bd351bb0bc3
Norhan-salem <norhansalem581@gmail.com>
2024-12-13 23:12:52 +0200

    intergrated db methods

--- lib/presentation/screens/add_edit_gift_screen.dart
+++ lib/presentation/screens/add_edit_gift_screen.dart
@@ -7,14 +7,19 @@ import 'package:hedieaty_flutter_application/presentation/widgets/custom_app_bar
 import 'package:hedieaty_flutter_application/presentation/widgets/details_input_text_field.dart';
 
 import '../../data/models/gift_model.dart';
+import '../../data/repositories/gift_repository.dart';
+import '../../data/services/gift_service.dart';
+import '../../domain/enums/GiftCategory.dart';
+import '../../domain/enums/GiftStatus.dart';
 import '../components/custom_dropdown_list.dart';
 import '../components/gift_status_toggle.dart';
 import '../components/img_upload.dart';
 
 class AddGiftScreen extends StatefulWidget {
+  final int eventId;
   final Gift? gift;
 
-  const AddGiftScreen({Key? key, this.gift}) : super(key: key);
+  AddGiftScreen({Key? key, required this.eventId, this.gift}) : super(key: key);
 
   @override
   _AddGiftScreenState createState() => _AddGiftScreenState();
@@ -26,8 +31,8 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
   late TextEditingController nameController;
   late TextEditingController descriptionController;
   late TextEditingController priceController;
-  String? category;
-  late String giftStatus;
+  GiftCategory? category;
+  GiftStatus? giftStatus;
   File? selectedImage;
 
   @override
@@ -37,10 +42,14 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
     nameController = TextEditingController(text: widget.gift?.name ?? '');
     descriptionController =
         TextEditingController(text: widget.gift?.description ?? '');
-    priceController =
-        TextEditingController(text: widget.gift?.price.toString() ?? '');
-    category = widget.gift?.category;
-    giftStatus = widget.gift?.status ?? 'Available';
+    priceController = TextEditingController(
+        text: widget.gift != null ? widget.gift!.price.toString() : '');
+    category = widget.gift != null
+        ? GiftCategory.values[widget.gift!.category]
+        : null;
+    giftStatus = widget.gift != null
+        ? GiftStatus.values[widget.gift!.status]
+        : GiftStatus.available;
   }
 
   @override
@@ -51,6 +60,57 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
     super.dispose();
   }
 
+  Future<void> saveGift() async {
+    try {
+      if (_addGiftFormKey.currentState!.validate()) {
+        final newGift = Gift(
+          id: widget.gift?.id ?? generateUniqueId(),
+          name: nameController.text,
+          giftImagePath: selectedImage?.path ??
+              widget.gift?.giftImagePath ??
+              'assets/images/gift_default_img.png',
+          description: descriptionController.text,
+          category: category?.index ?? 0,
+          price: double.parse(priceController.text),
+          status: giftStatus?.index ?? 0,
+          eventId: widget.eventId,
+          pledged_by_user_id: widget.gift?.pledged_by_user_id ?? '',
+          isDeleted: widget.gift?.isDeleted ?? false,
+          isPublished: widget.gift?.isPublished ?? false,
+        );
+        if (widget.gift == null) {
+          await GiftRepository().createGift(newGift);
+        } else {
+          final updateFields = {
+            'name': newGift.name,
+            'description': newGift.description,
+            'price': newGift.price,
+            'category': newGift.category,
+            'status': newGift.status,
+            'pledged_by_user_id': newGift.pledged_by_user_id,
+            'isPublished': newGift.isPublished,
+            'gift_image_path': newGift.giftImagePath,
+          };
+          await GiftRepository().updateGift(newGift.id, updateFields);
+        }
+        Navigator.pop(context, newGift);
+      }
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Failed to save the gift. Please try again.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
@@ -58,8 +118,8 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
     double padding = screenWidth * 0.07;
 
     return Scaffold(
-      appBar:
-          CustomAppBar(title: widget.gift == null ? 'Add Gift' : 'Edit Gift'),
+      appBar: CustomAppBar(
+          title: widget.gift == null ? 'Add Gift' : 'Edit Gift'),
       body: Stack(
         children: [
           BackgroundContainer(
@@ -71,7 +131,7 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
                   child: Column(
                     children: [
                       ImagePickerWidget(
-                        initialImagePath: widget.gift?.imageURL,
+                        initialImagePath: widget.gift?.giftImagePath,
                         onImageSelected: (image) {
                           setState(() {
                             selectedImage = image;
@@ -82,36 +142,33 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
                       DetailsTextField(
                         controller: nameController,
                         labelText: 'Gift Name',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a name';
+                          }
+                          return null;
+                        },
                       ),
                       DetailsTextField(
                         controller: descriptionController,
                         labelText: 'Description',
                       ),
                       CustomDropdownButton(
-                        category: category,
-                        items: [
-                          'Electronics',
-                          'Fashion and Accessories',
-                          'Home and Kitchen',
-                          'Books and Stationery',
-                          'Toys and Games',
-                          'Sports and Outdoors',
-                          'Beauty and Health',
-                          'Food and Beverages',
-                          'Experiences and Subscriptions',
-                          'Art and Craft Supplies',
-                          'Personalized Gifts',
-                          'Pet Supplies',
-                          'Gift Cards and Vouchers',
-                          'Other'
-                        ],
+                        category: category != null
+                            ? mapGiftCategoryToString(category!)
+                            : null,
+                        items: GiftCategory.values
+                            .map((e) => mapGiftCategoryToString(e))
+                            .toList(),
                         onChanged: (value) {
                           setState(() {
-                            category = value!;
+                            category = GiftCategory.values.firstWhere(
+                                  (e) => mapGiftCategoryToString(e) == value,
+                            );
                           });
                         },
                         validator: (value) =>
-                            value == null ? 'Select a category' : null,
+                        value == null ? 'Select a category' : null,
                       ),
                       DetailsTextField(
                         controller: priceController,
@@ -129,7 +186,7 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
                       ),
                       SizedBox(height: screenHeight * 0.02),
                       GiftStatusToggle(
-                        initialStatus: giftStatus,
+                        initialStatus: widget.gift?.giftStatus ?? GiftStatus.available,
                         onStatusChanged: (status) {
                           setState(() {
                             giftStatus = status;
@@ -138,23 +195,9 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
                       ),
                       SizedBox(height: screenHeight * 0.02),
                       CreateEventButton(
-                        onPressed: () {
-                          if (_addGiftFormKey.currentState!.validate()) {
-                            final newGift = Gift(
-                              name: nameController.text,
-                              description: descriptionController.text,
-                              category: category ?? 'Other',
-                              status: giftStatus,
-                              price: double.parse(priceController.text),
-                              imageURL: selectedImage?.path ??
-                                  'assets/images/gift_default_img.png',
-                            );
-
-                            Navigator.pop(context, newGift);
-                          }
-                        },
+                        onPressed: saveGift,
                         buttonText:
-                            widget.gift == null ? 'Add Gift' : 'Save Changes',
+                        widget.gift == null ? 'Add Gift' : 'Save Changes',
                       ),
                     ],
                   ),
@@ -166,4 +209,9 @@ class _AddGiftScreenState extends State<AddGiftScreen> {
       ),
     );
   }
+
+  generateUniqueId() {
+    return DateTime.now().millisecondsSinceEpoch;
+  }
 }
+
