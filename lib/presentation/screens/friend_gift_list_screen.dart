742cb277bab17c89e53421e18e7f685d7793298c
Norhan-salem <norhansalem581@gmail.com>
2024-12-13 23:13:29 +0200

    integrated gift fetching and saving

--- lib/presentation/screens/friend_gift_list_screen.dart
+++ lib/presentation/screens/friend_gift_list_screen.dart
@@ -1,63 +1,62 @@
 import 'package:flutter/material.dart';
 
 import '../../core/utils/sorting_menu_utils.dart';
+import '../../data/models/event_model.dart';
 import '../../data/models/gift_model.dart';
+import '../../data/repositories/gift_repository.dart';
 import '../components/friend_gift_list.dart';
 import '../widgets/background_image_container.dart';
 import '../widgets/custom_app_bar.dart';
 
 class FriendGiftsListScreen extends StatelessWidget {
-  final String eventName;
+  final Event event;
 
-  FriendGiftsListScreen({Key? key, required this.eventName}) : super(key: key);
+  FriendGiftsListScreen({Key? key, required this.event}) : super(key: key);
 
-  final List<Gift> gifts = [
-    Gift(
-        name: 'Smartphone',
-        category: 'Electronics',
-        status: 'Available',
-        price: 30.0,
-        description: ''),
-    Gift(
-        name: 'Book',
-        category: 'Education',
-        status: 'Pledged',
-        price: 30.0,
-        description: ''),
-    Gift(
-        name: 'Headphones',
-        category: 'Electronics',
-        status: 'Available',
-        price: 30.0,
-        description: ''),
-  ];
+  Future<List<Gift>> _fetchGifts(int eventId) async {
+    return await GiftRepository().fetchGiftsByEventId(eventId);
+  }
 
   @override
   Widget build(BuildContext context) {
-    ValueNotifier<List<Gift>> sortedGiftsNotifier =
-        ValueNotifier<List<Gift>>(gifts);
     double appBarPadding = MediaQuery.of(context).size.height * 0.02;
 
     return Scaffold(
       appBar: CustomAppBar(
-        title: "$eventName's Gifts",
+        title: "${event.name}'s Gifts",
         actionIcon: Icons.sort,
         onActionPressed: () {
-          showSortingMenu(context, sortedGiftsNotifier);
+          showSortingMenu(context, ValueNotifier<List<Gift>>([]));
         },
       ),
       body: BackgroundContainer(
         child: Padding(
           padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.05),
+            horizontal: MediaQuery.of(context).size.width * 0.05,
+          ),
           child: Column(
             children: [
               SizedBox(height: appBarPadding),
               Expanded(
-                child: ValueListenableBuilder<List<Gift>>(
-                  valueListenable: sortedGiftsNotifier,
-                  builder: (context, sortedGifts, child) {
-                    return FriendGiftList(friendGifts: sortedGifts);
+                child: FutureBuilder<List<Gift>>(
+                  future: _fetchGifts(event.id),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Failed to load gifts.'));
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return Center(child: Text('No gifts available.'));
+                    } else {
+                      final sortedGiftsNotifier = ValueNotifier<List<Gift>>(snapshot.data!);
+
+                      return ValueListenableBuilder<List<Gift>>(
+                        valueListenable: sortedGiftsNotifier,
+                        builder: (context, sortedGifts, child) {
+                          return FriendGiftList(friendGifts: sortedGifts);
+                        },
+                      );
+                    }
                   },
                 ),
               ),
@@ -68,3 +67,4 @@ class FriendGiftsListScreen extends StatelessWidget {
     );
   }
 }
+
