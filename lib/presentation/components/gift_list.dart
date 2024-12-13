af36d1dce272898f39d94e2a5c1d6c798be1dee0
Norhan-salem <norhansalem581@gmail.com>
2024-12-13 23:09:20 +0200

    implemented deletion

--- lib/presentation/components/gift_list.dart
+++ lib/presentation/components/gift_list.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:hedieaty_flutter_application/data/repositories/gift_repository.dart';
 
 import '../../data/models/gift_model.dart';
 import '../screens/add_edit_gift_screen.dart';
 import 'gift_tile.dart';
 
 class GiftList extends StatefulWidget {
+  final int eventId;
   final List<Gift> myGifts;
   final Function(Gift) onGiftAdded;
 
-  GiftList({required this.myGifts, required this.onGiftAdded});
+  GiftList({required this.myGifts, required this.onGiftAdded, required this.eventId});
 
   @override
   _GiftListState createState() => _GiftListState();
 }
 
 class _GiftListState extends State<GiftList> {
-  void _deleteGift(int index) {
-    setState(() {
-      widget.myGifts.removeAt(index);
-    });
+  Future<void> _deleteGift(int index) async {
+    try {
+      final giftId = widget.myGifts[index].id;
+      if (giftId != null) {
+        await GiftRepository().deleteGift(giftId);
+        setState(() {
+          widget.myGifts.removeAt(index);
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Gift deleted successfully!')),
+        );
+      } else {
+        throw Exception('Invalid gift ID.');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to delete gift: $e')),
+      );
+    }
   }
 
   void _updateGift(int index, Gift updatedGift) {
@@ -39,7 +56,7 @@ class _GiftListState extends State<GiftList> {
             Navigator.push<Gift>(
               context,
               MaterialPageRoute(
-                builder: (context) => AddGiftScreen(gift: gift),
+                builder: (context) => AddGiftScreen(gift: gift, eventId: widget.eventId),
               ),
             ).then((updatedGift) {
               if (updatedGift != null) {
@@ -48,7 +65,6 @@ class _GiftListState extends State<GiftList> {
             });
           },
           onDelete: () {
-            // Implement delete functionality
             _deleteGift(index);
           },
         );
