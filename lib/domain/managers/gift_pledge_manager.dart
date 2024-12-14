6b21ea94149e4dc8d0c8df040994ec14e6a2eaca
Norhan-salem <norhansalem581@gmail.com>
2024-12-14 23:35:28 +0200

    integrated db methods

--- lib/domain/managers/gift_pledge_manager.dart
+++ lib/domain/managers/gift_pledge_manager.dart
@@ -1,13 +1,35 @@
+import 'package:hedieaty_flutter_application/data/repositories/gift_repository.dart';
+import 'package:hedieaty_flutter_application/data/repositories/user_repository.dart';
+
 import '../../data/models/gift_model.dart';
+import '../enums/GiftStatus.dart';
 
 class GiftPledgeManager {
+  final GiftRepository _giftRepository = GiftRepository();
+  final UserRepository _userRepository = UserRepository();
+
   bool canPledgeGift(Gift gift) {
-    return gift.status != 1;
+    return gift.status == GiftStatus.available.index;
   }
 
-  void pledgeGift(Gift gift) {
-    if (canPledgeGift(gift)) {
-      gift.status = 1;
+  Future<bool> pledgeGift(Gift gift) async {
+    try {
+      if (!canPledgeGift(gift)) {
+        throw Exception('Gift is not available for pledging.');
+      }
+      final user = await _userRepository.fetchCurrentUser();
+      final rowsUpdated = await _giftRepository.pledgeGift(gift.id, user!.id);
+
+      if (rowsUpdated > 0) {
+        print('Gift pledged successfully.');
+        return true;
+      } else {
+        print('Failed to pledge the gift. It may have been modified or deleted.');
+        return false;
+      }
+    } catch (e) {
+      print('Error while pledging gift: $e');
+      return false;
     }
   }
 }
