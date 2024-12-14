1e5e9000ea1b195e987065b6c3dad863eac3e869
Norhan-salem <norhansalem581@gmail.com>
2024-12-14 23:36:22 +0200

    added rtdb url

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:hedieaty_flutter_application/presentation/screens/home_screen.dart';
 import 'package:hedieaty_flutter_application/presentation/screens/signup_screen.dart';
-import 'package:sqflite/sqflite.dart';
 import 'core/constants/color_palette.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -12,8 +10,11 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseDatabase.instance.databaseURL = 'https://hedeiaty-a4b2f-default-rtdb.europe-west1.firebasedatabase.app';
+
   runApp(MyApp());
 }
+
 
 
 class MyApp extends StatelessWidget {
