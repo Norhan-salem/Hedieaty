39c4a11b46995f4ec4ffe48fb4dac6319ee158e9
Norhan-salem <norhansalem581@gmail.com>
2024-12-17 17:19:56 +0200

    edited theme data tochange default loading color, text font family, overlay colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,53 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:hedieaty_flutter_application/presentation/screens/signup_screen.dart';
 import 'core/constants/color_palette.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'core/usecases/firebase_notification.dart';
 import 'firebase_options.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  FirebaseDatabase.instance.databaseURL = 'https://hedeiaty-a4b2f-default-rtdb.europe-west1.firebasedatabase.app';
-
+  await Firebase.initializeApp();
+  //await NotificationService.instance.initialize();
   runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
 
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hedieaty',
        theme: ThemeData(
+         scaffoldBackgroundColor: ColorPalette.eggShell,
+         progressIndicatorTheme: ProgressIndicatorThemeData(
+           color: ColorPalette.darkTeal
+         ),
          highlightColor: Colors.teal.withOpacity(0.2),
          textSelectionTheme: TextSelectionThemeData(
            cursorColor: ColorPalette.darkTeal,
+         ),
+         buttonTheme: ButtonThemeData(
+           splashColor: ColorPalette.darkCyan,
+           highlightColor: ColorPalette.darkCyan,
+         ),
+         textButtonTheme: TextButtonThemeData(
+           style: TextButton.styleFrom(
+             splashFactory: InkSplash.splashFactory,
+             overlayColor: ColorPalette.darkCyan,
+           ),
+         ),
+         elevatedButtonTheme: ElevatedButtonThemeData(
+           style: ElevatedButton.styleFrom(
+             splashFactory: InkSplash.splashFactory,
+             overlayColor: ColorPalette.darkCyan,
+           ),
+         ),
+         outlinedButtonTheme: OutlinedButtonThemeData(
+           style: OutlinedButton.styleFrom(
+             splashFactory: InkSplash.splashFactory,
+             overlayColor: ColorPalette.darkCyan,
+           ),
          ),
         appBarTheme: AppBarTheme(
           backgroundColor: ColorPalette.lightYellow,
@@ -40,6 +60,7 @@ class MyApp extends StatelessWidget {
           ),
           iconTheme: IconThemeData(color: ColorPalette.darkTeal),
         ),
+         fontFamily: 'Poppins',
       ),
       home: SignUpScreen(),
     );
